ce5e38f791454d2ff499f30cf324f5df7cebd990
Christelle Nkera <scnkera@gmail.com>
2024-10-28 14:33:58 -0400

    Updated activity

--- activity/original.py
+++ activity/original.py
@@ -1,2 +1,22 @@
 def uncommon_from_sentences(s1, s2):
-    pass
+
+    word_count = {}
+
+    for word in s1.split():
+        word_count[word] = word_count.get(word, 0) + 1
+
+    for word in s2.split():
+        word_count[word] = word_count.get(word, 0) + 1
+
+    uncommon_word = []
+    for key, value in word_count.items():
+        if value == 1:
+            uncommon_word.append(key)
+        
+    return uncommon_word
+    
+
+print(uncommon_from_sentences("Ice cream you scream we all scream for ice cream", "Hi how are you"))
+
+
+
